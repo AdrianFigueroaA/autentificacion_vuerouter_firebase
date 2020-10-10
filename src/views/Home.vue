b306da062bab97df628e497830202adafdbd9657
adrian <revanskyw@gmail.com>
2020-10-10 00:30:40 -0300

    funciona

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Hola soy el Home</h1>
+    <button @click="logout">Cerrar Sesion</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import firebase from "firebase";
+export default {
+  name: "Home",
+
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .singOut()
+        .then(() => this.$router.replace('login'));
+    },
+  },
+};
+</script>
